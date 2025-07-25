58dcf0f96101d962f89a192a90e50b5769a4e4c5
Atif Raza <atif.raza@ad.iserveu.co.in>
2025-07-25 14:21:24 +0530

    created login API

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,39 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/register",async(req,res)=>{
+     const data = req.body;
+     if (!data || Object.keys(data).length === 0) {
+        return res.status(400).json({ error: 'Request body is empty' });
+    }
+
+    if(!req.body.name || !req.body.password || !req.body.email){
+        return res.status(400).json({ error: 'Missing required fields: name or email' });
+    }
     let user = new User(req.body);
     console.log(req.body);
     let result = await user.save();
+    delete result.password;
     console.log(result)
     res.json({result});
 })
+
+app.post("/login",async(req,res)=>{
+     const data = req.body;
+     if (!data || Object.keys(data).length === 0) {
+        return res.status(400).json({ error: 'Request body is empty' });
+    }
+
+    if(!req.body.password || !req.body.email){
+        return res.status(400).json({ error: 'Missing required fields: email or password' });
+    }
+    let user = await User.findOne(req.body);
+    console.log(user);
+    if(user){
+        res.json({user});
+    }
+    else{
+        return res.json({ error: 'No User Found' });
+    }
+;})
 
 app.listen(5000);
